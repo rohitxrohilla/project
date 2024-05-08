f4233765d98e4d8b9a20340e509c738b2dadb3d7
rohitxrohilla <rohitxrohilla@gmail.com>
2024-05-08 10:41:17 +0530

    commit on project

---
+++ project/111/practise.py
@@ -0,0 +1,18 @@
+# list=[1,2,3,4,5,6,7,8,9,10]
+# n=5
+# for i in list:
+#     table=n*i
+#     print(table)
+
+# list=[10,30,23,43,65,12]
+# sum=0
+# for i in list:
+#     sum=sum+i
+# print("sum is",sum)
+
+# for i in range(10):
+#     print(i,end= " ")
+
+
+
+    
